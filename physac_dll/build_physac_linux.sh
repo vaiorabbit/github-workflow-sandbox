9dc7c77df2fae6a24b36153b20e18569d316069e
vaiorabbit <vaiorabbit>
2023-01-22 18:07:01 +0900

    Update

--- physac_dll/build_physac_linux.sh
+++ physac_dll/build_physac_linux.sh
@@ -4,3 +4,4 @@ cd build
 cmake -D CMAKE_C_FLAGS="" CMAKE_BUILD_TYPE=Release -D BUILD_SHARED_LIBS=ON -D BUILD_EXAMPLES=OFF -D CMAKE_C_COMPILER=clang ..
 make
 cp -R physac.so ../../lib/physac.so
+ls -alR
